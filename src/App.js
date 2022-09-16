0443c56519dfded59dec99dd203f22f261d29b35
NeeMu1017 <102221983+NeeMu1017@users.noreply.github.com>
2022-09-16 14:02:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useRef, useState, useEffect } from "react";
+
+function App (){
+    const [name , setName] = useState ("");
+    const [age , setAge] = useState ("");
+    const [gender , setGender] = useState ("");
+    const [email,setEmail] =useState ("");
+     
+    function onSubmitHnadeler(){
+        alert (`Name is${name}      Age is ${age}          Gender is ${gender}          email is${email} `);
+    }
+
+    return(
+        <div className="container">
+            <h2 className="text-center">Some infromtion About You</h2>
+              <form onSubmit={onSubmitHnadeler}>
+                <div className="md-3">
+                <label className="form-label fw-bolder">Name</label>    
+                 <input className="form-control" placeholder="Name "name="name" type="text" value={name} onChange={(event)=>setName (event.target.value)} />
+                </div>
+                <div className="mt-3">
+                <label className="form-label fw-bolder ">Date of birth</label>    
+                 <input  placeholder="Age " name="age" type="date" value={age} onChange={(event)=>setAge (event.target.value)} />
+                </div>
+                <div>
+                    <label className="form-label fw-bolder">Gender</label>
+                </div>
+                <div>
+                    <input type="radio" name="gender" value='male' onChange={(event)=> setGender(event.target.value)}/>
+                    <label className="form-label">Male</label>
+                    <input type="radio" name="gender" value='fe male' onChange={(event)=> setGender(event.target.value)}/>
+                    <label className="form-label">Female</label>
+                </div>
+                <div>   
+                    <label className="form-label fw-bolder">Email</label>
+                    <input placeholder="email" className="" type='email'value={email} onChange={(event)=>setEmail (event.target.value)}/>
+                </div>
+                <input type='submit' value='Submit'/>
+                
+            
+           
+            
+        </form>
+        </div>
+      
+    )
 }
-
-export default App;
+export default App
